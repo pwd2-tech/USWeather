e00346e778a2514d138299f712afee9692e0a68c
myUserName <pwld2@example.com>
2026-02-24 21:14:29 +0000

    Commit Dataviz repo

---
+++ eq_explore_data.py
@@ -0,0 +1,44 @@
+import json
+
+from plotly.graph_objs import Scattergeo, Layout
+from plotly import offline
+
+# Explore the structure of the data.
+filename = 'data/eq_data_1_day_m1.json'
+with open(filename) as f:
+    all_eq_data = json.load(f)
+
+all_eq_dicts = all_eq_data['features']
+print(len(all_eq_dicts))
+
+mags, lons, lats = [], [], []
+for eq_dict in all_eq_dicts:
+    mag = eq_dict['properties']['mag']
+    lon = eq_dict['geometry']['coordinates'][0]
+    lat = eq_dict['geometry']['coordinates'][1]
+    mags.append(mag)
+    lons.append(lon)
+    lats.append(lat)
+
+print(mags[:10])
+print(lons[:5])
+print(lats[:5])
+
+# Map the earthquakes
+#data = [{Scattergeo(lon = lons, lat = lats)]
+data = [{
+    'type': 'scattergeo',
+    'lon': lons,
+    'lat': lats,
+    'marker':{
+        'size':[5*mag for mag in mags],
+    },
+}]
+my_layout = Layout(title='Global Earthquakes')
+
+fig = {'data':data, 'layout':my_layout}
+offline.plot(fig, filename='global_earthquakes.html')
+
+readable_file = 'data/readable_eq_data.json'
+with open(readable_file, 'w') as f:
+    json.dump(all_eq_data, f, indent=4)
---
+++ show_color_scales.py
@@ -0,0 +1,6 @@
+from plotly import colors
+
+for key in colors.PLOTLY_SCALES:
+    print(key)
+
+print(colors.PLOTLY_SCALES)
